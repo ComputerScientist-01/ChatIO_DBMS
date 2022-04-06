b233a7497b65b2db609e3c9f0ee9a81a2c5e3e12
ComputerScientist-01 <rathi406@gmail.com>
2022-04-06 14:06:56 +0530

    Updated frontend UI

---
+++ frontend/public/js/main.js
@@ -0,0 +1,59 @@
+const chatForm = document.getElementById('chat-form')
+const msg = document.getElementById('msg')
+const chatDiv = document.querySelector('.chat-messages')
+const usersList = document.getElementById('users')
+
+const roomname = document.getElementById('room-name').innerText
+const username = document.getElementById('user-name').innerHTML
+const room = document.getElementById('room-name').innerHTML
+
+const socket = io()
+
+// Join chatroom
+socket.emit('joinRoom', { username, room })
+
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomUsers(room, users)
+})
+
+// Message submit from client
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    socket.emit('chatMessage', msg.value)
+    msg.value = ''
+    msg.focus();
+})
+
+//  Receiving message from server
+socket.on('message', (msg) => {
+    displayMessage(msg)
+    chatDiv.scrollTop = chatDiv.scrollHeight;
+})
+
+socket.on('type', (res) => {
+    document.getElementById('typing').innerHTML = res
+})
+
+function displayMessage(msg) {
+    const div = document.createElement('div');
+    div.classList.add('message')
+    div.innerHTML = `
+        <p class="meta">${msg.username}<span>&nbsp;&nbsp;&nbsp;&nbsp;${msg.time}</span></p>
+	    <p class="text">${msg.text}</p>
+    `
+    chatDiv.appendChild(div)
+}
+
+function outputRoomUsers(room, users) {
+    document.getElementById('room-name').innerText = room;
+    usersList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')}
+        `;
+}
+
+function onkeyFn() {
+    socket.emit('starttype', `${username} is typing...`)
+    setTimeout(() => {
+        socket.emit('stoptype', "")
+    }, 2000);
+}
